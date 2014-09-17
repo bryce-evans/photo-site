f405b45fe7f0222e65a36268365a49b5e4ceb8d8
bae43 <bae43@cornell.edu>
2014-09-17 01:28:16 -0400

    added basic files for photo downloads

---
+++ photoshare/myset/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link ref='stylesheet' src ='../css/download.css'/>
+		<title>(Set Name) - Download</title>
+	</head>
+	<body>
+		<ul>
+			<li>
+				<img src='thumbs/Sunset.jpg'/>
+			</li>
+			<li>
+				<img src='thumbs/Blue hills.jpg'/>
+			</li>
+			<li>
+				<img src='thumbs/Winter.jpg'/>
+			</li>
+			<li>
+				<img src='thumbs/Water lilies.jpg'/>
+			</li>
+		</ul>
+		<div>
+			My Set
+			<div>
+				Date - ### photos
+			</div>
+		</div>
+		<button>
+			Download
+		</button>
+		<button>
+			Select Images
+		</button>
+		<div>
+			Footer
+		</div>
+		<div>
+			Logo
+		</div>
+</html>
