bef5af3046a760fdce380b7aec4d503f122495b1
bae43 <bae43@cornell.edu>
2014-10-01 11:23:08 -0400

    started upload.php and styles

---
+++ photos/upload.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link rel='stylesheet' href ='../css/download.css'/>
+		<link rel='stylesheet' href ='../../css/fonts.css'/>
+		<title>Uploader | Bryce Evans Photography</title>
+	</head>
+	<body>
+		<div id='page'>
+			<ul id='film-reel'>
+				<li>
+					<div style='background-image: url("tiles/tile-1.jpg")'/>
+				</li>
+				<li>
+					<div style='background-image: url("tiles/tile-2.jpg")'/>
+				</li>
+				<li>
+					<div style='background-image: url("tiles/tile-3.jpg")'/>
+				</li>
+				<li>
+					<div style='background-image: url("tiles/tile-4.jpg")'/>
+				</li>
+			</ul>
+			<div id="content">
+			<div id="title">
+				<span id='main'>Uploader</span>
+				<span id='subtitle'>v.0.01</span>
+			</div>
+
+			<div id='drop-zone'>
+				<button id='download-button'  class="green button">Upload</button>
+			</div>
+			<div class='logo-positioner'>
+<a class='logo-container' href='/'> <img  src='../logos/small.png'></img>  </a></div>
+		</div>
+		</div>
+		<div id='footer'>
+			&copy;2014 Bryce A. Evans
+		</div>
+	</body>
+</html>
